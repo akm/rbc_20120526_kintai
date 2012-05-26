d11ac70e57d0ee3ed11a1badd203bd2f44c4c803
akm <akima@groovenauts.jp>
2012-05-26 15:14:09 +0900

    Personの名前を指定せずに保存したらエラーになるようにspecを書きました。

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "名前なしで保存したらエラーになるべし" do
+    person = Person.create(:name => "")
+    person.errors.should_not be_empty
+    person.persisted?.should == false
+  end
 end
