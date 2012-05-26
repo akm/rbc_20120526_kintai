423bca5024d437d5e0d399c6564b5adeb281f63a
akm <akima@groovenauts.jp>
2012-05-26 17:01:27 +0900

    PeopleControllerから参照されるPersonモデルのstartメソッド、finishメソッドw実装しました。

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,17 @@ class Person < ActiveRecord::Base
   attr_accessible :name
 
   validates :name, :presence => true
+
+  def start
+    works.create!(:started_at => Time.now)
+  end
+
+  def finish
+    work = works.where(:finished_at => nil).first
+    # work.update_attributes!(:finished_at => Time.now)
+    work.finished_at = Time.now
+    work.save!
+  end
+
+
 end
