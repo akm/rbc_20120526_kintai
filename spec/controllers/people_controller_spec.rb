ef5201475aa48002abcd779ac00cb457ef1cd0c5
akm <akima@groovenauts.jp>
2012-05-26 15:18:28 +0900

    モデルにバリデーションを追加したのでコントローラのspecが失敗するようになったためコントローラのspecを修正しました

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -24,7 +24,9 @@ describe PeopleController do
   # Person. As you add validations to Person, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {
+      :name => "foo"
+    }
   end
   
   # This should return the minimal set of values that should be in the session
