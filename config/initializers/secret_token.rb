7df0cb38765fc191dbc80388be79c7510e57b237
akm <akima@groovenauts.jp>
2012-05-26 14:13:17 +0900

    initial commit $ rails new Kintai -T -d sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kintai::Application.config.secret_token = '6ebb67745a78c181d836cd84eb2b621ca7b3566e63454302774a61fb721fbf642972250b83c4a9c066b3e38577e91d796812816859e26a0833ca9a408194fc6c'
